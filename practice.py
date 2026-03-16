4a8bd3e2daa98a15121ced41d4d5afcfad82f711
jaeho1900 <glad1900@naver.com>
2026-03-17 08:35:10 +0900

    123

--- practice.py
+++ practice.py
@@ -1,12 +1,20 @@
-def  print_5xn(i):
-    print(i[j])
+def  print_5xn(a):
+    if len(a)%5 == 0:
+        k = len(a)//5
+    else:
+        k = len(a)//5+1
+    for i in range(k):
+        print(a[0:5])
+        a = a[5:]
 
-print_5xn("아이엠어보이유알어걸")
+def print_5xn(line):
+    chunk_num = int(len(line) / 5)
+    for x in range(chunk_num + 1) :
+        print(line[x * 5: x * 5 + 5])
 
-a = "아이엠어보이유알어걸"
-a[]
+def print_5xn(line):
+    # 0부터 시작해서 5씩 건너뛰며 반복
+    for i in range(0, len(line), 5):
+        print(line[i : i + 5])
 
-for i in a:
-
-    for k ==5:
-        a
+print_5xn("아이256엠078어보이유알어걸")
