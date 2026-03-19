5b8a5be1c7eb2468f5952feb7ae28681e29a7452
jaeho1900 <glad1900@naver.com>
2026-03-19 17:18:57 +0900

    123

--- practice.py
+++ practice.py
@@ -1,20 +1,21 @@
-def  print_5xn(a):
-    if len(a)%5 == 0:
-        k = len(a)//5
-    else:
-        k = len(a)//5+1
-    for i in range(k):
-        print(a[0:5])
-        a = a[5:]
+def convert_int(string):
+    return int(string.replace(',',''))
 
-def print_5xn(line):
-    chunk_num = int(len(line) / 5)
-    for x in range(chunk_num + 1) :
-        print(line[x * 5: x * 5 + 5])
+convert_int("1,234,567")
 
-def print_5xn(line):
-    # 0부터 시작해서 5씩 건너뛰며 반복
-    for i in range(0, len(line), 5):
-        print(line[i : i + 5])
+import datetime
 
-print_5xn("아이256엠078어보이유알어걸")
+now = datetime.datetime.now()
+print(now.timedelta(day))
+
+
+import datetime
+
+now = datetime.datetime.now()
+
+print(now.strftime("%H:%M:%S"))
+
+
+
+print(datetime.datetime.strptime("2020-05-04", "%Y-%m-%d"))
+
