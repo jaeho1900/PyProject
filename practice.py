3110f7012817c1f6b7cf4ab98a74116c7fe07195
jaeho1900 <glad1900@naver.com>
2026-03-24 08:30:22 +0900

    123

--- practice.py
+++ practice.py
@@ -1,21 +1,17 @@
-def convert_int(string):
-    return int(string.replace(',',''))
+class Human:
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+    def __del__(self):
+        print("나의 죽음을 알리지마라")
+    def who(self):
+        print("이름:", self.name, "나이:", self.age, "성별:", self.sex)
+    def setInfo(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
 
-convert_int("1,234,567")
-
-import datetime
-
-now = datetime.datetime.now()
-print(now.timedelta(day))
-
-
-import datetime
-
-now = datetime.datetime.now()
-
-print(now.strftime("%H:%M:%S"))
-
-
-
-print(datetime.datetime.strptime("2020-05-04", "%Y-%m-%d"))
-
+areum = Human("조아름", 25, "여자")
+areum.who()
+del areum
