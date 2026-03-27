6f12e93247ad7c5e71c4da7942f49a86349defcd
jaeho1900 <glad1900@naver.com>
2026-03-27 09:17:46 +0900

    123

--- practice.py
+++ practice.py
@@ -1,17 +1,14 @@
-class Human:
-    def __init__(self, name, age, sex):
+class Stock:
+    def __init__(self, name, code, per, pbr, rate):
         self.name = name
-        self.age = age
-        self.sex = sex
-    def __del__(self):
-        print("나의 죽음을 알리지마라")
-    def who(self):
-        print("이름:", self.name, "나이:", self.age, "성별:", self.sex)
-    def setInfo(self, name, age, sex):
-        self.name = name
-        self.age = age
-        self.sex = sex
+        self.code = code
+        self.per = per
+        self.pbr = pbr
+        self.rate = float(rate)
 
-areum = Human("조아름", 25, "여자")
-areum.who()
-del areum
+
+
+삼성 = Stock("삼성전자", "005930")
+삼성.get_name()
+삼성.get_code()
+삼성.name
