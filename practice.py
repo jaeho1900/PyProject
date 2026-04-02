451dba491b0e87cd76f54fd7437785fa1566ad68
jaeho1900 <glad1900@naver.com>
2026-04-03 08:26:28 +0900

    123

--- practice.py
+++ practice.py
@@ -1,14 +1,27 @@
 class Stock:
-    def __init__(self, name, code, per, pbr, rate):
+    def __init__(self, name, code, per, pbr, 배당수익률):
         self.name = name
         self.code = code
         self.per = per
         self.pbr = pbr
-        self.rate = float(rate)
+        self.배당수익률 = 배당수익률
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_code(self, code):
+        self.code = code
+
+    def get_name(self):
+        return self.name
+
+    def get_code(self):
+        return self.code
 
 
+삼성 = Stock("삼성전자", "005930", 15.79, 1.33, 2.83)
+현대 = Stock("현대차","005380",	8.70, 0.35, 4.27)
+LG = Stock("LG전자","066570",	317.34,	0.69,	1.37)
 
-삼성 = Stock("삼성전자", "005930")
-삼성.get_name()
-삼성.get_code()
-삼성.name
+for i in [삼성, 현대, LG]:
+    print(i.code, i.per)
