098433edc8d3d2e6b4b5297b2538b601e6befdbe
jaeho1900 <glad1900@naver.com>
2026-04-28 12:28:21 +0900

    123

--- practice.py
+++ practice.py
@@ -1,6 +1,13 @@
+import random
 class Account():
     def __init__ (bank_name, accounter_name, account_num, balance):
-        self.bank_name = bank_name            # sc은행
+        self.bank_name = "sc은행"
         self.accounter_name = accounter_name  # 3-2-6 랜덤
         self.account_num = account_num
         self.balance = balance
+       random.randrange(100,1000)
+       random.randrange(10,100)
+       random.randrange(100000,1000000)
+
+
+print(f"str(random.randrange(100,1000))-str(random.randrange(10,100))-str(random.randrange(100000,1000000))")
