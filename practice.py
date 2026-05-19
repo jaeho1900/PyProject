bd29f532123717e5db03c293c3ed23a1e8f15486
jaeho1900 <glad1900@naver.com>
2026-05-19 09:22:48 +0900

    123

--- practice.py
+++ practice.py
@@ -1,6 +1,7 @@
 import random
 
 class Account:
+    account_count = 0
     def __init__ (self, name, balance):
         self.name = name
         self.balance = balance
@@ -12,12 +13,22 @@ class Account:
         num2 = str(num2).zfill(2)
         num3 = str(num3).zfill(6)
         self.num = num1 + '-' + num2 + '-' + num3
+        Account.account_count +=1
+
+    @classmethod
+    def get_account_num(cls):
+        print(cls.account_count)
+
+    def deposit(self, income):
+        if income >= 1:
+            self.balance += income
 
 k = Account("tomy", 9990)
+j = Account("jam", 9990)
 k.name
+j.name
+Account.account_count
+k.get_account_num()
+j.get_account_num()
+k.deposit(100000)
 k.balance
-k.bank
-k.num
-
-
-
