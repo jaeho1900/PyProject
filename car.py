e0d84b9f64600922dc252a4383a6075d9a53a2e3
jaeho1900 <glad1900@naver.com>
2026-06-26 10:33:22 +0900

    123

--- car.py
+++ car.py
@@ -1,13 +1,9 @@
-# 다음 코드가 동작하도록 차 클래스를 정의하세요.
-
-# >> car = 차(2, 1000)
-# >> car.바퀴
-# 2
-# >> car.가격
-# 1000
-
 class 차:
-    def __init__ (self, tier_num, price):
+    def __init__(self, tier_num, price):
         self.바퀴 = tier_num
         self.가격 = price
+
+car = 차(2, 1000)
+car.바퀴
+car.가격
 
