548007ce66094ecc80dc50816cb3b4f1223ac3f4
jaeho1900 <glad1900@naver.com>
2026-07-07 08:28:27 +0900

    123

--- car.py
+++ car.py
@@ -5,8 +5,14 @@ class 차:
 
 class 자전차(차):
     def __init__(self, 바퀴, 가격, 구동계):
-        super().__init__(바퀴, 가격)
+        차.__init__(self, 바퀴, 가격)
+        # super().__init__(바퀴, 가격)
         self.구동계 = 구동계
+
+class 자동차(차):
+    def 정보(self):
+        print(f'바퀴수 {self.바퀴}')
+        print(f'가격 {self.가격}')
 
 car = 차(2, 1000)
 car.바퀴
@@ -18,8 +24,5 @@ bicycle.가격
 bicycle = 자전차(2, 100, "시마노")
 bicycle.구동계
 
-# 285. 다음 코드가 동작하도록 차 클래스를 상속받는 자동차 클래스를 정의하세요.
-# >> car = 자동차(4, 1000)
-# >> car.정보()
-# 바퀴수 4
-# 가격 1000
+car = 자동차(4, 1000)
+car.정보()
