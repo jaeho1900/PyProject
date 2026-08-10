6c5fb46de9884e556a50cf54d3bb4294a8f12400
jaeho1900 <glad1900@naver.com>
2026-08-11 08:32:59 +0900

    123

---
+++ practice.py
@@ -0,0 +1,15 @@
+a = 2
+b = 3.14
+c = a+b
+print(a)
+print(b)
+print(c)
+print(f'a의 값은 {a}입니다.')
+print(f'{c:.2f}')
+print('%.2f'%(c))
+print('{:.2f}'.format(c))
+d = 1e3
+e = 1e-3
+print(b**a)
+a==2
+a=3
